42efab2f4e3d47d7c58cfc7808a2cd9f49d290a2
joeltonmats <joeltonmatos@gmail.com>
2017-11-15 20:08:32 -0400

    defined grunt tasks and server. Grunt will serve a dist folder

--- GruntFile.js
+++ GruntFile.js
@@ -10,6 +10,88 @@ module.exports = function (grunt) {
   // Grunt configuration
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    // copy files -----------------------------------
+    copy: {
+      production: {
+        files: [{ expand: true, cwd: './src', src: ['**'], dest: 'dist/' },],
+      }
+    },
+
+    // minify css files -----------------------------------
+    cssmin: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      production: {
+        files: [{
+          expand: true, cwd: 'src', src: ['**/*.css', '!*.min.css'], dest: 'dist', ext: '.min.css'
+        }]
+      }
+    },
+
+    // minify js files -----------------------------------
+    uglify: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      production: {
+        files: [{
+          cwd: 'src',
+          expand: true,
+          src: ['**/*.js', '!**/*.min.js'],
+          dest: 'dist',
+          rename: function (dst, src) {
+            return dst + '/' + src.replace('.js', '.min.js');
+          }
+        }]
+      }
+    },
+
+    // clean unecessary files -----------------------------------
+    clean: {
+      production: {
+        src: ['dist/**/*.js', 'dist/**/*.css', '!dist/**/*.min.js', '!dist/**/*.min.css']
+      }
+    },
+
+    // comment js and css and uncomment .min.js and .min.css -----------------------------------
+    replace: {
+      production: {
+        options: {
+          patterns:
+          [
+            { match: /<!--dev build-->/g, replacement: '<!--dev build' },
+            { match: /<!--end dev build-->/g, replacement: 'end dev build-->' },
+            { match: /<!--production build/g, replacement: '<!--production build-->' },
+            { match: /end production build-->/g, replacement: '<!--end production build-->' },
+          ]
+        },
+        files: [{ expand: true, cwd: 'src', src: ['**/*.html'], dest: 'dist/' }]
+      }
+    },
+
+    // minify html files -----------------------------------
+    htmlmin: {
+      production: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'dist',
+          src: ['**/*.html'],
+          dest: 'dist'
+        }]
+      }
+    },
+
+    watch: {
+      files: ['src/**/*.html', 'src/**/*.js', 'src/**/*.css'],
+      tasks: ['copy:production', 'cssmin:production', 'uglify:production', 'clean:production', 'replace:production', 'htmlmin:production']
+    },
+
     pagespeed: {
       options: {
         nokey: true,
@@ -27,24 +109,12 @@ module.exports = function (grunt) {
         }
       }
     },
-
-    // minify css files -----------------------------------
-    cssmin: {
-      options: {
-        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
-      },
-      production: {
-        files: [{
-          expand: true, cwd: './css', src: ['*.css', '!*.min.css'], dest: './css', ext: '.min.css'
-        }]
-      }
-    },
   });
 
   // Register customer task for ngrok
   grunt.registerTask('psi-ngrok', 'Run pagespeed with ngrok', function () {
     var done = this.async();
-    var port = 8081;
+    var port = 3000;
 
     ngrok.connect(port, function (err, url) {
       if (err !== null) {
@@ -59,4 +129,5 @@ module.exports = function (grunt) {
 
   // Register default tasks
   grunt.registerTask('default', ['psi-ngrok']);
+  grunt.registerTask('production', ['copy:production', 'cssmin:production', 'uglify:production', 'clean:production', 'replace:production', 'htmlmin:production']);
 };
